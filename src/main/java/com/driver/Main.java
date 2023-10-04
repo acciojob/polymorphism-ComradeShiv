647e30613b1e6ebfdc9b64d0bbdc0201f4bed5b2
= <=>
2023-10-04 09:55:40 +0530

    attempt 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+    public static class product{
+
+        public int product(int x, int y) {
+            return x * y;
+        }
+
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        product p = new product();
+
+        System.out.println(p.product(5, 4));
+        System.out.println(p.product(5, 4, 3));
+        System.out.println(p.product(5.0, 4.0));
+    }
 }
